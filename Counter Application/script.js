5ae81c425ed881072fa92494b5e9b820aa67db90
Asiama Phoebe <phoebeasiama6@gmail.com>
2025-01-02 19:17:36 +0000

    script

--- Counter Application/script.js
+++ Counter Application/script.js
@@ -1,8 +1,24 @@
 let increase = document.getElementById('increase');
 let reset = document.getElementById('reset');
 let decrease = document.getElementById('decrease');
-let value = document.getElementById('value');
-increase.onclick = function() {
-    let current = parseInt(value.textContent);
-    count.textContent = current + 1;
-}
+let value = document.querySelector('#value');
+ 
+const btns = document.querySelectorAll('.btn');
+btns.forEach(btn=>{
+    btn.addEventListener('click', function(e){
+        e.preventDefault();
+        let val = parseInt(value.textContent);
+        switch(this.id){
+            case 'increase':
+                val++;
+                break;
+            case 'decrease':
+                val--;
+                break;
+            case'reset':
+                val = 0;
+                break;
+        }
+        value.textContent = val;
+    })
+})
